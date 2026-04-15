9464cfb768531c71bd0ba4408f0b83675d27077a
Michael Ding <yandy.ding@gmail.com>
2026-04-15 17:37:48 +0800

    rm useless info

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -69,19 +69,6 @@ vim.api.nvim_create_autocmd("FileType", {
                     end
                     return vim.split(args_string, " +")
                 end,
-
-                -- 💀
-                -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-                --
-                --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-                --
-                -- Otherwise you might get the following error:
-                --
-                --    Error on launch: Failed to attach to the target process
-                --
-                -- But you should be aware of the implications:
-                -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-                -- runInTerminal = false,
             },
         }
 
